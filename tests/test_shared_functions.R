98903ca18750fc80ccd040813b6bd1d24103750b
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-08-11 16:55:31 +0100

    linted another test file

--- tests/test_shared_functions.R
+++ tests/test_shared_functions.R
@@ -1,11 +1,11 @@
 # This file stores common functions that are shared across multiple test scripts
 
-test_for_error <- function(code, expected_regexp = '.+') {
+test_for_error <- function(code, expected_regexp = ".+") {
   tryCatch({
     code
     return("No error returned")
   }, error = function(e) {
-    if(grepl(expected_regexp, e$message)) return(TRUE)
+    if (grepl(expected_regexp, e$message)) return(TRUE)
     return(c(e$message, "Expected error did not match - ", expected_regexp))
   }
   )
