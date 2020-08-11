8441d37252403702e979030e76cc6465ab0c21c0
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-08-11 09:33:14 +0100

    separated repeating function into shared functions file, to reduce duplication

--- tests/test_corp_text.R
+++ tests/test_corp_text.R
@@ -2,18 +2,8 @@ library(CorporaCoCo)
 library(data.table)
 library(unittest, quietly=TRUE)
 
-
-test_for_error <- function(code, expected_regexp = '.+') {
-    tryCatch({
-            code
-            return("No error returned")
-        }, error = function(e) {
-            if(grepl(expected_regexp, e$message)) return(TRUE)
-            return(c(e$message, "Expected error did not match - ", expected_regexp))
-        }
-    )
-}
-
+# Retrieve functions that are shared across multiple test files
+source('tests/test_shared_functions.R')
 
 # -----
 # tests
