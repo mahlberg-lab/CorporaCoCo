ae3d40107886dd6a6b02c7eb372131b96dbaa101
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-08-12 13:44:13 +0100

    did some basic linting of coco.R to see if prevents tests from passing

--- R/coco.R
+++ R/coco.R
@@ -1,12 +1,12 @@
 .coco <- function(A, B, nodes, fdr = 0.01, collocates = NULL) {
-    if(! 'data.frame' %in% class(A)) stop("'A' must be a data.frame.")
-    if(! 'data.frame' %in% class(B)) stop("'B' must be a data.frame.")
-    if(! identical(c(x = 'character', y = 'character', H = 'integer', M = 'integer'), sapply(A, class))) stop("'A' looks wrong. Please check the required form of A.")
-    if(! identical(c(x = 'character', y = 'character', H = 'integer', M = 'integer'), sapply(B, class))) stop("'B' looks wrong. Please check the required form of B.")
-    if(! is.character(nodes) | length(nodes) < 1) stop("'nodes' must be a character vector of at least length one.")
-    if(! is.numeric(fdr) || length(fdr) > 1) stop("'fdr' must be a single number.")
-    if(fdr <= 0.0 || fdr > 1.0) stop("'fdr' must be greater than zero and less than or equal to zero.")
-    if(length(collocates) != 0 && ! is.character(collocates)) stop("'collocates' must be a character vector.")
+    if (! 'data.frame' %in% class(A)) stop("'A' must be a data.frame.")
+    if (! 'data.frame' %in% class(B)) stop("'B' must be a data.frame.")
+    if (! identical(c(x = 'character', y = 'character', H = 'integer', M = 'integer'), sapply(A, class))) stop("'A' looks wrong. Please check the required form of A.")
+    if (! identical(c(x = 'character', y = 'character', H = 'integer', M = 'integer'), sapply(B, class))) stop("'B' looks wrong. Please check the required form of B.")
+    if (! is.character(nodes) | length(nodes) < 1) stop("'nodes' must be a character vector of at least length one.")
+    if (! is.numeric(fdr) || length(fdr) > 1) stop("'fdr' must be a single number.")
+    if (fdr <= 0.0 || fdr > 1.0) stop("'fdr' must be greater than zero and less than or equal to zero.")
+    if (length(collocates) != 0 && ! is.character(collocates)) stop("'collocates' must be a character vector.")
      
     # hack to stop R CMD check warnings
     x = y = H = M = H_A = H_B = M_A = M_B = p_adjusted = p_value = i.T = NULL
@@ -18,7 +18,7 @@
     setkey(B, x, y)
 
     # filter by collocates
-    if(length(collocates) != 0) {
+    if (length(collocates) != 0) {
         A <- A[y %in% collocates] 
         B <- B[y %in% collocates] 
     }
@@ -40,7 +40,7 @@
 
     DT <- na.omit(DT)  # will get NA's for y if a node is not in either x
 
-    if(nrow(DT) > 0) {
+    if (nrow(DT) > 0) {
         # Fisher
         # calculating the confidence interval is expensive so we will only do it for the significant values
         DT[, c('p_value', 'effect_size', 'CI_lower', 'CI_upper') := fisher(H_A, H_B, M_A, M_B), by = 1:nrow(DT)]
