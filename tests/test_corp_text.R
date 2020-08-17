69c03f518fb4da85cdce4963bfa3b033eba880e3
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-08-17 12:22:33 +0100

    renamed test_shared_functions.R to shared_functions.R to highlight that it's not actually testing a file called shared_functions.R like the other test files

--- tests/test_corp_text.R
+++ tests/test_corp_text.R
@@ -3,7 +3,7 @@ library(data.table)
 library(unittest, quietly = TRUE)
 
 # Retrieve functions that are shared across multiple test files
-source(file = "test_shared_functions.R", local = TRUE)
+source(file = "shared_functions.R", local = TRUE)
 
 # -----
 # tests
