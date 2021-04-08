e5636a69556d223dbbf20c9977e621a6493312ae
edmondac-admin <a.c.edmondson@bham.ac.uk>
2021-04-08 15:50:51 +0100

    Converted test to using testthat

--- tests/testthat/test-corp_concordance.R
+++ tests/testthat/test-corp_concordance.R
@@ -1,15 +1,4 @@
-library(CorporaCoCo)
-library(data.table)
-library(unittest, quietly = TRUE)
-
-# Retrieve functions that are shared across multiple test files
-source(file = "shared_functions.R", local = TRUE)
-
-# -----
-# tests
-# -----
-
-ok_group("corp_concordance", {
+test_that("corp_concordance", {
     x <- paste("`But do cats eat bats, I wonder?' And here Alice began to get rather sleepy, and went on saying to herself, in a dreamy sort of way,",
                "`Do cats eat bats? Do cats eat bats?' and sometimes, `Do bats eat cats?'",
                "for, you see, as she couldn't answer either question, it didn't much matter which way she put it.",
@@ -18,12 +7,18 @@ ok_group("corp_concordance", {
     # this is silly but it tests if we can concordance every instance of every type
     text <- corp_text(x)
     got <- corp_concordance(text, span = "2L3R")
-    ok(nrow(got) == 59, "with no filtering there is a concordance for every token")
-    got <- corp_concordance(text, span = "2L3R", nodes = "bats")
-    ok(nrow(got) == 4, "node filtering")
-    got <- corp_concordance(text, span = "2L3R", collocates = "bats")
-    ok(nrow(got) == 16, "collocate filtering")
+    # with no filtering there is a concordance for every token
+    expect_equal(nrow(got), 59)
 
+    # node filtering
+    got <- corp_concordance(text, span = "2L3R", nodes = "bats")
+    expect_equal(nrow(got), 4)
+
+    # collocate filtering
+    got <- corp_concordance(text, span = "2L3R", collocates = "bats")
+    expect_equal(nrow(got), 16)
+
+    # corp_concordance.corp_surface
     counts <- corp_surface(text, span = "2L3R")
-    ok(identical(corp_concordance(counts), corp_concordance(text, span = "2L3R")), "corp_concordance.corp_surface")
+    expect_identical(corp_concordance(counts), corp_concordance(text, span = "2L3R"))
 })
